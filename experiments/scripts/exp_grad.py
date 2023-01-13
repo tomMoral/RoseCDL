a198a7dc00ffa2f889cc8551016e36c34e9213a3
Cedric Allain <cedric.allain31@gmail.com>
2023-01-13 16:52:39 +0100

    update exp_grad

--- experiments/scripts/exp_grad.py
+++ experiments/scripts/exp_grad.py
@@ -52,7 +52,7 @@ def compute_grad(X, z, D, i=None, W=1_000, extended=False):
 
     n_trials, n_channels, n_times = X.shape
 
-    if i is None:
+    if i is None:  # compute full gradient
         _, grad = _l2_gradient_d(D, X, z, constants=None)
         return grad
 
@@ -62,46 +62,13 @@ def compute_grad(X, z, D, i=None, W=1_000, extended=False):
     # pad with (L-1) zeros on both sides of the last dim
     z_pad = np.pad(z, ((0, 0), (0, 0), (L-1, L-1)), constant_values=0)
     i += L - 1
-
     # shape (n_atoms, n_trials, W+L-1)
     z_w_ext = z_pad[:, :, (i-L+1):(i+W)].copy()
     # shape (n_trials, n_channels, W+2*L-2)
     X_hat = construct_X_multi(z_w_ext, D=D, n_channels=n_channels)
-    X_hat = X_hat[:, :, L:W+L]  # shape (n_trials, n_channels, W)
+    X_hat = X_hat[:, :, L-1:L-1+W]  # shape (n_trials, n_channels, W)
     residual = X_w - X_hat
     grad = _dense_transpose_convolve_z(residual, z_w)
-    return grad
-
-    # if extended:  # pad with (L-1) zeros on both sides of the last dim
-    #     X = np.pad(X, ((0, 0), (0, 0), (L-1, L-1)), constant_values=0)
-    #     z = np.pad(z, ((0, 0), (0, 0), (L-1, L-1)), constant_values=0)
-
-    # if i is not None:
-    #     assert i <= (T-W), \
-    #         f"i must be 0 <= i <= (T - W) = {T - W}, got i = {i}"
-
-    #     if not extended:
-    #         X = X[:, :, i:(i+W)].copy()      # shape (n_trials, n_channels, W)
-    #         z = z[:, :, i:(i+W-L+1)].copy()  # shape (n_atoms, n_trials, W-L+1)
-    #     else:
-    #         i += L - 1
-    #         # shape (n_trials, n_channels, W+2*L-2)
-    #         X = X[:, :, (i-L+1):(i+W+L-1)].copy()
-    #         # shape (n_atoms, n_trials, W+L-1)
-    #         z = z[:, :, (i-L+1):(i+W)].copy()
-
-    # # compute constants$
-    # n_trials, n_channels, n_times = X.shape
-    # XtX = np.dot(X.ravel(), X.ravel())
-    # reg = 0.1
-
-    # _, ztz, ztX = update_z_multi(
-    #     X, D, reg, z0=None, solver='l-bfgs', solver_kwargs=dict(),
-    #     return_ztz=True, n_jobs=1)
-    # constants = dict(n_channels=n_channels, XtX=XtX, ztz=ztz, ztX=ztX)
-
-    # _, grad = _l2_gradient_d(D, X, z, constants=None)
-
     return grad
 
 
@@ -110,21 +77,47 @@ X, D, z = simulate_data(
     n_acti_atom=n_acti_atom, random_state=42, constant_amplitude=False,
     window=True, shapes=['sin', 'gaussian'], sigma_noise=1, plot_atoms=False)
 
+n_trials, n_channels, n_times = X.shape
+
+
+def test_convolve(z, D, W, i=0):
+
+    L = D.shape[-1]
+
+    X_hat = construct_X_multi(z, D=D, n_channels=n_channels)
+    X_hat = X_hat[:, :, i:i+W]
+    # pad
+    z_pad = np.pad(z, ((0, 0), (0, 0), (L-1, L-1)), constant_values=0)
+    i += L-1
+    z_w_ext = z_pad[:, :, (i-L+1):(i+W)].copy()
+    X_hat_ext = construct_X_multi(z_w_ext, D, n_channels=n_channels)
+    X_hat_ = X_hat_ext[:, :, L-1:L-1+W].copy()
+    residual = X_hat - X_hat_
+
+    return norm(residual)
+
+
 # compute full grad
-full_grad = compute_grad(X, z, D, i=None, extended=False)
+full_grad = compute_grad(X, z, D, i=None)
 # compute list of windowed grads
-W = 1_000
-win_grads = np.array([compute_grad(X, z, D, i=this_i*W, W=W, extended=False)
+W = int(T/10)
+
+win_grads = np.array([compute_grad(X, z, D, i=this_i*W, W=W)
                       for this_i in range(T//W)])
-print(f'norm of diff: {norm(win_grads.sum(axis=0)/W - full_grad/T)}')
+print(f'norm of diff: {norm(win_grads.mean(axis=0)/W - full_grad/T)}')
+
+
 # %%
+T = 10_000
+W = 1_000
+list_W = np.arange(W, T+W, W)
+list_L = [50, 100, 500]
+list_p_acti = [1, 5, 10]
 
 dict_error = []
 dict_esp = []
 
 for L in list_L:
-    list_W = np.arange(2*L, T-L, L)
-
     for p_acti in list_p_acti:
         n_acti_atom = int(T/L * p_acti)
 
@@ -133,61 +126,106 @@ for L in list_L:
             n_acti_atom=n_acti_atom, random_state=42, constant_amplitude=False,
             window=True, shapes=['sin', 'gaussian'], sigma_noise=1, plot_atoms=False)
 
+        full_grad = compute_grad(X, z, D)
+
         for W in list_W:
             # compute grad on window partition
             list_i_part = [i*W for i in range(T//W)]
             n_win = len(list_i_part)
+
+            win_grad = np.array([compute_grad(X, z, D, i, W)
+                                 for i in list_i_part])
+            dict_esp.append({
+                'L': L, 'p_acti': p_acti, 'W': W,
+                'error_esp': norm(win_grad.mean(axis=0)/W - full_grad/T)}
+            )
+            print(f"norm of diff: {dict_esp[-1]['error_esp']}")
+
             # get 20 random indices
-            list_i_rnd = np.random.choice((T-W-L), 20, replace=True) + L
+            # list_i_rnd = np.random.choice((T-W-L), 20, replace=True) + L
 
-            for is_extended in [True, False]:
-                full_grad = compute_grad(X, z, D, extended=is_extended)
-                rel_full_grad = full_grad / (T + is_extended * 2 * (L-1))
+            # for is_extended in [False]:
+            #     full_grad = compute_grad(X, z, D, extended=is_extended)
+            #     rel_full_grad = full_grad / (T + is_extended * 2 * (L-1))
 
-                for is_partition, this_list_i in \
-                        zip([True, False], [list_i_part, list_i_rnd]):
+            # for is_partition, this_list_i in \
+            #         zip([True], [list_i_part]):
 
-                    win_grad = np.array([compute_grad(
-                        X, z, D, i, W, extended=is_extended)
-                        for i in this_list_i])
-                    # compute error to full grad
-                    dict_error.extend([{
-                        'L': L, 'p_acti': p_acti, 'partition': is_partition,
-                        'W': W, 'extended': is_extended,
-                        'error': norm(this_win_grad/(W + is_extended*2*(L - 1)) - rel_full_grad)}
-                        for this_win_grad in win_grad])
+            #     win_grad = np.array([compute_grad(
+            #         X, z, D, i, W, extended=is_extended)
+            #         for i in this_list_i])
+            #     # compute error to full grad
+            #     dict_error.extend([{
+            #         'L': L, 'p_acti': p_acti, 'partition': is_partition,
+            #         'W': W, 'extended': is_extended,
+            #         'error': norm(this_win_grad/(W + is_extended*2*(L - 1)) - rel_full_grad)}
+            #         for this_win_grad in win_grad])
+            #     dict_esp.append({
+            #         'L': L, 'p_acti': p_acti, 'partition': is_partition,
+            #         'W': W, 'extended': is_extended,
+            #         'error_esp': norm(win_grad.sum(axis=0) - full_grad)}
+            #     )
 
-df_err = pd.DataFrame(dict_error)
-df_err.to_csv('../results/df_exp_grad')
+# df_err = pd.DataFrame(dict_error)
+# df_err.to_csv('../results/df_exp_grad')
+df_esp = pd.DataFrame(dict_esp)
+sns.relplot(
+    data=df_esp,
+    x="W", y="error_esp",
+    col="L", row="p_acti",
+    kind="line"
+)
+plt.show()
 
 # %%
 # Varying p_acti and partitioning (fixed L)
-L = 100
-g = sns.relplot(
-    data=df_err[(df_err['L'] == L) & (df_err['W'] >= min(list_W))],
-    x="W", y="error",
-    col="partition", row="p_acti",
-    hue="extended",
-    kind="line"
-)
-(g.set_axis_labels('W', 'L2 error norm')
-  .tight_layout()
- )
-plt.xscale('log')
-plt.xlim(min(list_W), None)
-plt.subplots_adjust(top=0.9)
-plt.suptitle(
-    f'L2 norm between sub-window gradient and full signal one \n(T={T:.1e}, L={L})')
-plt.savefig('../figures/exp_grad_partition_pacti.pdf')
-plt.show()
+# L = 100
+# g = sns.relplot(
+#     data=df_err[(df_err['L'] == L) & (df_err['W'] >= min(list_W))],
+#     x="W", y="error",
+#     col="partition", row="p_acti",
+#     hue="extended",
+#     kind="line"
+# )
+# (g.set_axis_labels('W', 'L2 error norm')
+#   .tight_layout()
+#  )
+# plt.xscale('log')
+# plt.xlim(min(list_W), None)
+# plt.subplots_adjust(top=0.9)
+# plt.suptitle(
+#     f'L2 norm between sub-window gradient and full signal one \n(T={T:.1e}, L={L})')
+# plt.savefig('../figures/exp_grad_partition_pacti.pdf')
+# plt.show()
 
+
+# # Varying L and p_acti (partition or random)
+# partition = True
+# g = sns.relplot(
+#     data=df_err[(df_err['partition'] == partition)
+#                 & (df_err['W'] >= min(list_W))],
+#     x="W", y="error",
+#     col="L", row="p_acti",
+#     hue="extended",
+#     kind="line"
+# )
+# (g.set_axis_labels('W', 'L2 error norm')
+#   .tight_layout()
+#  )
+# plt.xscale('log')
+# plt.xlim(min(list_W), None)
+# plt.subplots_adjust(top=0.9)
+# plt.suptitle(
+#     f"L2 norm between {'partition' if partition else 'random'} sub-window gradient and full signal one \n(T={T:.1e})")
+# plt.savefig('../figures/exp_grad_L_pacti.pdf')
+# plt.show()
 
 # Varying L and p_acti (partition or random)
-partition = False
+partition = True
 g = sns.relplot(
-    data=df_err[(df_err['partition'] == partition)
-                & (df_err['W'] >= min(list_W))],
-    x="W", y="error",
+    data=df_esp[(df_esp['partition'] == partition)
+                & (df_esp['W'] >= min(list_W))],
+    x="W", y="error_esp",
     col="L", row="p_acti",
     hue="extended",
     kind="line"
