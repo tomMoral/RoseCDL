058b14a05e7a949e0cc5347a4c85be7194603662
Thomas Moreau <thomas.moreau.2010@gmail.com>
2025-01-15 13:46:00 +0100

    TST add test on subwindowing and outlier_mask (#17)

---
+++ wincdl/tests/test_masking.py
@@ -0,0 +1,22 @@
+import torch
+
+from wincdl.utils.utils_outliers import get_outlier_mask
+
+
+def test_get_outlier_mask_1d():
+    start_outlier = 5
+    len_sig, len_outlier = 100, 2
+
+    outliers = slice(start_outlier, start_outlier + len_outlier+1)
+    err = torch.zeros(1, 1, len_sig)
+    err[0, 0, outliers] = 1
+
+    mask = get_outlier_mask(err, mehod="quantile", threshold=2 * len_outlier / len_sig)
+    assert torch.all(mask == err)
+
+    opening_window = 3
+    mask = get_outlier_mask(err, mehod="quantile", threshold=0.8, opening_window=opening_window)
+    expected_mask = torch.zeros(1, 1, len_sig)
+    masked_times = slice(start_outlier-opening_window+1, start_outlier+len_outlier+1)
+    expected_mask[0, 0, masked_times] = 1
+    assert torch.all(mask == expected_mask)
---
+++ wincdl/tests/test_windows.py
@@ -0,0 +1,38 @@
+import pytest
+
+import numpy as np
+from wincdl.datasets.subwindow_dataset import SubwindowsDataset
+
+class TestSubwindowsDataset:
+    @pytest.mark.parametrize("n_trials", [1, 2])
+    @pytest.mark.parametrize("sample_window", [None, 5])
+    def test_1d_no_window(self, n_trials, sample_window):
+        n_times = 10
+        data = np.zeros((n_trials, 1, n_times))
+        dataset = SubwindowsDataset(data, sample_window=sample_window)
+
+        n_windows = n_times - sample_window + 1 if sample_window is not None else 1
+        expected_len = n_trials if sample_window is None else n_trials * n_windows
+        assert len(dataset) == expected_len
+
+        if sample_window is not None:
+            for i in range(len(dataset)):
+                assert dataset[i].shape == (1, sample_window)
+
+    @pytest.mark.parametrize("n_trials", [1, 2])
+    @pytest.mark.parametrize("sample_window", [None, (5, 4)])
+    def test_2d_no_window(self, n_trials, sample_window):
+        support = (10, 11)
+        data = np.zeros((n_trials, 1, *support))
+        dataset = SubwindowsDataset(data, sample_window=sample_window)
+
+        n_windows = np.prod(
+            tuple(s - sw + 1 for s, sw in zip(support, sample_window))
+            if sample_window is not None else 1
+        )
+        expected_len = n_trials if sample_window is None else n_trials * n_windows
+        assert len(dataset) == expected_len
+
+        if sample_window is not None:
+            for i in range(len(dataset)):
+                assert dataset[i].shape == (1, *sample_window)
