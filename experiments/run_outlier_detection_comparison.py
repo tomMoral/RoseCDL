46a3791df08b192aabc2661ed236803c4dde4b2a
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-01-15 12:06:07 +0100

    docstring in run_one() and extend run config list

--- experiments/run_outlier_detection_comparison.py
+++ experiments/run_outlier_detection_comparison.py
@@ -5,14 +5,12 @@ plot_outliers_detection.py to generate the plots.
 
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from joblib import Memory, Parallel, delayed
 from torch import cuda
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-
-from joblib import Parallel, delayed
-from joblib import Memory
 
 from wincdl.utils.utils_exp import (
     evaluate_D_hat,
@@ -23,16 +21,38 @@ from wincdl.utils.utils_exp import (
 from wincdl.utils.utils_signal import generate_experiment
 from wincdl.wincdl import WinCDL
 
-
 mem = Memory(location="__cache__", verbose=0)
 
 
-EXP_DIR = Path("results")
-EXP_DIR.mkdir(exist_ok=True, parents=True)
-
-
 @mem.cache
-def run_one(method, outliers_kwargs, wincdl_params, simulation_params, i, seed, exp_dir):
+def run_one(
+    cdl_package: str,
+    outlier_detection_method: dict[str, str or float],
+    outlier_detection_timing: str,
+    outliers_kwargs: dict[str, str or float],
+    cdl_params: dict[str, str or float],
+    simulation_params: dict[str, str or float],
+    seed: float,
+    i: int,
+):
+    """Run the experiment for a given CDL package and outlier detection method.
+
+    Args:
+        cdl_package (str): CDL package name: "wincdl", "alphacsc" or "sporco".
+        outlier_detection_method (str): Outlier detection method.
+            A dictionary is expected with two keys:
+                - "name": name of the method. Can be one of "quantile", "iqr",
+                    "zscore", "mad" or "none".
+                - "alpha" (float): Parameter of the method.
+        outlier_detection_timing (str): When to run the outlier detection
+            relatively to the CDL: "before", "during" or "never".
+        outliers_kwargs (dict): Additional parameters for outlier detection.
+        cdl_params (dict): Parameters for the CDL algorithm.
+        simulation_params (dict): Parameters for data simulation.
+        seed (float): Random seed.
+        i (int):
+
+    """
 
     # Generate the data
     simulation_params["rng"] = seed
@@ -52,12 +72,11 @@ def run_one(method, outliers_kwargs, wincdl_params, simulation_params, i, seed, 
         method = {}
         this_outliers_kwargs = None
     else:
-        this_outliers_kwargs = dict(
-            **outliers_kwargs, **method
-        )
+        this_outliers_kwargs = dict(**outliers_kwargs, **method)
 
     # Setup the callback
     results = []
+
     def callback_fn(model, epoch, loss):
         if hasattr(model.loss_fn, "method"):
             metrics = get_outliers_metric(
@@ -67,17 +86,25 @@ def run_one(method, outliers_kwargs, wincdl_params, simulation_params, i, seed, 
             metrics = {}
         recovery_score = evaluate_D_hat(D_true, model.D_hat_)
 
-        results.append({
-            "name": method_name,
-            **method, **info_contam, **metrics,
-            "recovery_score": recovery_score,
-            "seed": seed, "epoch": epoch, "loss": loss
-        })
+        results.append(
+            {
+                "name": method_name,
+                **method,
+                **info_contam,
+                **metrics,
+                "recovery_score": recovery_score,
+                "seed": seed,
+                "epoch": epoch,
+                "loss": loss,
+            }
+        )
 
     # Run the experiment
     wincdl = WinCDL(
-        **wincdl_params, D_init=D_init, outliers_kwargs=this_outliers_kwargs,
-        callbacks=[callback_fn]
+        **wincdl_params,
+        D_init=D_init,
+        outliers_kwargs=this_outliers_kwargs,
+        callbacks=[callback_fn],
     )
     wincdl.fit(X)
 
@@ -92,17 +119,30 @@ def run_one(method, outliers_kwargs, wincdl_params, simulation_params, i, seed, 
 if __name__ == "__main__":
 
     import argparse
+
     parser = argparse.ArgumentParser(
-        description='Run a comparison of the different methods for dictionary recovery'
+        description="Run a comparison of the different methods for dictionary recovery"
     )
-    parser.add_argument('--n-jobs', '-j', type=int, default=1,
-                        help='Number of parallel jobs')
-    parser.add_argument('--seed', '-s', type=int, default=None,
-                        help='Master seed for reproducible job')
-    parser.add_argument('--n-runs', '-n', type=int, default=20,
-                        help='Number of repetitions for the experiment')
-    parser.add_argument('--reg', type=float, default=0.8,
-                        help='Regularization parameter')
+    parser.add_argument(
+        "--n-jobs", "-j", type=int, default=1, help="Number of parallel jobs"
+    )
+    parser.add_argument(
+        "--seed",
+        "-s",
+        type=int,
+        default=None,
+        help="Master seed for reproducible job",
+    )
+    parser.add_argument(
+        "--n-runs",
+        "-n",
+        type=int,
+        default=20,
+        help="Number of repetitions for the experiment",
+    )
+    parser.add_argument(
+        "--reg", type=float, default=0.8, help="Regularization parameter"
+    )
     args = parser.parse_args()
 
     DEVICE = "cuda" if cuda.is_available() else "cpu"
@@ -116,7 +156,6 @@ if __name__ == "__main__":
 
     n_runs = args.n_runs
     reg = args.reg
-
 
     exp_dir = EXP_DIR / f"outlier_detection_reg_{reg}"
     exp_dir.mkdir(exist_ok=True, parents=True)
@@ -173,29 +212,120 @@ if __name__ == "__main__":
     }
 
     # Define list of methods to test
-    alpha_true = 0.1
-    list_methods = [
-        None,
-        {"method": "quantile", "alpha": alpha_true * 2},
-        {"method": "quantile", "alpha": alpha_true},
-        {"method": "quantile", "alpha": alpha_true / 2},
+
+    run_config_list = [
+        {
+            "cdl_package": "wincdl",
+            "outlier_detection_method": {"name": "none", "alpha": -1.0},
+            "outlier_detection_timing": "never",
+        },
+        {
+            "cdl_package": "wincdl",
+            "outlier_detection_method": {"name": "quantile", "alpha": 0.05},
+            "outlier_detection_timing": "before",
+        },
+        {
+            "cdl_package": "wincdl",
+            "outlier_detection_method": {"name": "quantile", "alpha": 0.1},
+            "outlier_detection_timing": "before",
+        },
+        {
+            "cdl_package": "wincdl",
+            "outlier_detection_method": {"name": "quantile", "alpha": 0.2},
+            "outlier_detection_timing": "before",
+        },
+        {
+            "cdl_package": "wincdl",
+            "outlier_detection_method": {"name": "quantile", "alpha": 0.05},
+            "outlier_detection_timing": "before",
+        },
+        {
+            "cdl_package": "wincdl",
+            "outlier_detection_method": {"name": "quantile", "alpha": 0.1},
+            "outlier_detection_timing": "before",
+        },
+        {
+            "cdl_package": "wincdl",
+            "outlier_detection_method": {"name": "quantile", "alpha": 0.2},
+            "outlier_detection_timing": "before",
+        },
+    ]
+    cdl_package_list = ["wincdl", "alphacsc", "sporco"]
+    outlier_detection_method_list = [
+        {"method": "none", "alpha": -1},
+        {"method": "quantile", "alpha": 0.05},
+        {"method": "quantile", "alpha": 0.1},
+        {"method": "quantile", "alpha": 0.2},
         {"method": "iqr", "alpha": 1.5},
         {"method": "zscore", "alpha": 1},
         {"method": "zscore", "alpha": 2},
-        # {"method": "zscore", "alpha": 3},
-        # {"method": "mad", "alpha": 1},
         {"method": "mad", "alpha": 3.5},
     ]
+    outlier_detection_timing_list = ["before", "during", "never"]
+
+    def allowed_detection_timings(
+        cdl_package: str, outlier_detection_timing_list: list[str]
+    ):
+        if cdl_package == "wincdl":
+            return outlier_detection_timing_list
+        return [
+            timing
+            for timing in outlier_detection_timing_list
+            if timing != "during"
+        ]
+
+    def allowed_detection_methods(
+        outlier_detection_timing: str, outlier_detection_method_list: list[str]
+    ) -> list[str]:
+        if outlier_detection_timing == "never":
+            return [
+                method
+                for method in outlier_detection_method_list
+                if method["name"] == "none"
+            ]
+        return [
+            method
+            for method in outlier_detection_method_list
+            if method["name"] != "none"
+        ]
+
+    def generate_run_config_list(
+        cdl_package_list: list,
+        outlier_detection_method_list: list,
+        outlier_detection_timing_list: list,
+    ):
+        run_config_list = []
+        for package in cdl_package_list:
+            for timing in allowed_detection_timings(
+                package, outlier_detection_timing_list
+            ):
+                for method in allowed_detection_methods(
+                    timing, outlier_detection_method_list
+                ):
+                    run_config_list.append(
+                        {"package": package, "method": method, "timing": timing}
+                    )
+        return run_config_list
 
     list_seeds = rng.integers(0, 2**32 - 1, n_runs)
     results = Parallel(n_jobs=args.n_jobs, return_as="generator_unordered")(
         delayed(run_one)(
-            this_method, outliers_kwargs, wincdl_params, simulation_params, i, seed, exp_dir
+            this_method,
+            outliers_kwargs,
+            wincdl_params,
+            simulation_params,
+            i,
+            seed,
+            exp_dir,
         )
         for i, seed in enumerate(list_seeds)
         for this_method in list_methods
     )
-    results = list(r for res in tqdm(results, "Running", total=n_runs*len(list_methods)) for r in res)
+    results = list(
+        r
+        for res in tqdm(results, "Running", total=n_runs * len(list_methods))
+        for r in res
+    )
 
     # Save results
     df_results = pd.DataFrame(results)
