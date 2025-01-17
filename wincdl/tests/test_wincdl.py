50f5afaa348cd1a7bc627807893f4de2ce42e624
Jad <64837518+Jad-yehya@users.noreply.github.com>
2025-01-17 11:48:14 +0100

    ENH add positive_D parameter to constrain dictionary to be positive (#19)
    Co-authored-by: tommoral <thomas.moreau.2010@gmail.com>

---
+++ wincdl/tests/test_wincdl.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pytest
+from wincdl.wincdl import WinCDL
+
+
+@pytest.mark.parametrize(
+    "kernel_size, support",
+    [
+        (32, (100,)),  # 1D
+        ((8, 9), (40, 64)),  # 2D
+    ],
+)
+# TODO: Add "uv_constraint" to possible rank values
+@pytest.mark.parametrize("rank", ["full"])
+@pytest.mark.parametrize("n_channels", [1, 3])
+@pytest.mark.parametrize("n_components", [3, 5])
+@pytest.mark.parametrize("positive_D", [True, False])
+def test_wincdl(
+    n_components, n_channels, kernel_size, support, rank, positive_D
+):
+    """Test positive dictionary constraint with 1D and 2D data"""
+    # Setup test data
+    X = np.random.rand(10, n_channels, *support)
+
+    # Initialize WinCDL and fit it
+    cdl = WinCDL(
+        n_components=n_components,
+        kernel_size=kernel_size,
+        n_channels=n_channels,
+        positive_D=positive_D,
+        epochs=1,
+        lmbd=0.1,
+        rank=rank,
+    )
+    cdl.fit(X)
+
+    # Assert the shape of the dictionary
+    kernel_size = (kernel_size,) if isinstance(kernel_size, int) else kernel_size
+    assert cdl.D_hat_.shape == (n_components, n_channels, *kernel_size), (
+        "Dictionary shape is not correct. Expected: "
+        f"{(n_components, n_channels, *kernel_size)}, got: {cdl.D_hat_.shape}"
+    )
+
+    # If positive_D is True, assert that all values in the dictionary are positive
+    if positive_D:
+        assert np.all(cdl.D_hat_ >= 0), (
+            f"{len(support)}D dictionary contains negative values, while using positive_D=True"
+        )
