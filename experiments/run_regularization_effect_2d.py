9335a8bad770d841453b1db6ad561f78be44b56a
Jad-yehya <jadyehya@hotmail.com>
2025-01-20 11:17:44 +0100

    ENH refactor run_regularization_effect_2d.py

--- experiments/run_regularization_effect_2d.py
+++ experiments/run_regularization_effect_2d.py
@@ -10,12 +10,12 @@ from tqdm import tqdm
 from joblib import Parallel, delayed
 from joblib import Memory
 
-from wincdl.datasets import create_conv_dataloader
-from wincdl.utils.utils_exp import evaluate_D_hat, make_size
+from wincdl.datasets import create_dataloader
+from wincdl.utils.utils_exp import evaluate_D_hat
 from wincdl.wincdl import WinCDL
 
 mem = Memory(location="__cache__", verbose=0)
-EXP_DIR = Path("results")
+EXP_DIR = Path("results") / "run_regularization_effect_2d"
 EXP_DIR.mkdir(exist_ok=True, parents=True)
 
 
@@ -25,37 +25,36 @@ def run_one(reg, run_idx, model_params, data_params, seed):
     # rng = np.random.default_rng(seed + run_idx)
 
     # Create dataloader
-    dataloader = create_conv_dataloader(
+    dataloader = create_dataloader(
         data=data_params["X"].reshape(1, 1, *data_params["X"].shape),
         sample_window=model_params["sample_window"],
-        dtype=model_params["dtype"],
+        # dtype=model_params["dtype"],
         device=model_params["device"],
     )
 
-    # Setup callback
+    # Setup the callback for monitoring and visualization
     results = []
+    D_true = data_params["true_D"]
 
-    def callback_recovery(model, epoch, loss):
-        true_D = make_size(data_params["true_D"], model.D_hat_.shape)
-        recovery_score = evaluate_D_hat(
-            model.D_hat_,
-            true_D,
-        )
+    def callback_fn(model, epoch, loss):
+        score = evaluate_D_hat(D_true[:, None], model.D_hat_)
+
         results.append(
             {
                 "run_idx": run_idx,
                 "epoch": epoch,
                 "loss": loss,
-                "recovery_score": recovery_score,
                 "reg": reg,
+                "score": score,
+                "seed": seed,
             }
         )
 
     # Setup and train model
     model = WinCDL(
-        **model_params,
+        callbacks=[callback_fn],
         lmbd=reg,
-        callbacks=[callback_recovery],
+        **model_params,
     )
     model.fit(dataloader)
     return results
@@ -66,6 +65,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description="Analyze regularization effect on dictionary learning"
+    )
+    # Add the path to the data file as an argument
+    parser.add_argument(
+        "--data-file", "-d", type=str, help="Path to the data file", required=True
     )
     parser.add_argument(
         "--n-jobs", "-j", type=int, default=1, help="Number of parallel jobs"
@@ -86,13 +89,18 @@ if __name__ == "__main__":
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Device: {DEVICE}")
 
-    seed = args.seed
-    if seed is None:
-        seed = np.random.randint(0, 2**32 - 1)
-    print(f"Seed: {seed}")
+    master_seed = args.seed
+    if master_seed is None:
+        master_seed = np.random.randint(0, 2**32 - 1)
+    print(f"Master seed: {master_seed}")
+
+    # Generate unique seeds for each run
+    rng = np.random.RandomState(master_seed)
+    seeds = rng.randint(0, 2**32 - 1, size=args.n_runs)
+    print(f"Generated {len(seeds)} unique seeds for runs")
 
     # Load data
-    data = np.load("text_4_5000_ICML.npz")
+    data = np.load(args.data_file)
     data_params = {"X": data.get("X"), "true_D": data.get("D")}
 
     # Model parameters
@@ -100,23 +108,23 @@ if __name__ == "__main__":
         "n_components": 6,
         "kernel_size": (35, 35),
         "n_channels": 1,
-        "lmbd": 0.1,
-        "n_iterations": 30,
-        "epochs": 100,
-        "sample_window": 500,
+        "n_iterations": 60,
+        "epochs": 30,
+        "sample_window": 50000,
         "mini_batch_size": 10,
         "device": DEVICE,
-        "dtype": torch.float32,
+        # "dtype": torch.float32,
+        "scale_lmbd": False,
     }
 
     # Experiment parameters
-    reg_values = [0.01, 0.1] #, 0.2, 0.3, 0.4, 0.5, 0.6]
+    reg_values = [0.01, 0.1 , 0.2, 0.3, 0.4, 0.5, 0.6]
     exp_dir = EXP_DIR / "reg_effect"
     exp_dir.mkdir(exist_ok=True, parents=True)
 
     # Run experiments
     results = Parallel(n_jobs=args.n_jobs, return_as="generator_unordered")(
-        delayed(run_one)(reg, run_idx, model_params, data_params, seed)
+        delayed(run_one)(reg, run_idx, model_params, data_params, seeds[run_idx])
         for reg in reg_values
         for run_idx in range(args.n_runs)
     )
