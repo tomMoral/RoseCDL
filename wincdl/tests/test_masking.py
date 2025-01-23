4cceefc8d64f61675e4f1da2e03f12120523ac4e
Jad-yehya <jadyehya@hotmail.com>
2025-01-23 18:28:45 +0100

    TST Param removed

--- wincdl/tests/test_masking.py
+++ wincdl/tests/test_masking.py
@@ -11,11 +11,11 @@ def test_get_outlier_mask_1d():
     err = torch.zeros(1, 1, len_sig)
     err[0, 0, outliers] = 1
 
-    mask = get_outlier_mask(err, mehod="quantile", threshold=2 * len_outlier / len_sig)
+    mask = get_outlier_mask(err, threshold=2 * len_outlier / len_sig)
     assert torch.all(mask == err)
 
     opening_window = 3
-    mask = get_outlier_mask(err, mehod="quantile", threshold=0.8, opening_window=opening_window)
+    mask = get_outlier_mask(err, threshold=0.8, opening_window=opening_window)
     expected_mask = torch.zeros(1, 1, len_sig)
     masked_times = slice(start_outlier-opening_window+1, start_outlier+len_outlier+1)
     expected_mask[0, 0, masked_times] = 1
