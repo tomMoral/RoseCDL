167e4016e142e21f900dde6316f5d684fbe20f92
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-01-31 15:28:48 +0100

    test init csc1d

---
+++ tests/test_csc/test_dim1.py
@@ -0,0 +1,38 @@
+import torch
+
+from rosecdl.csc.dim1 import CSC1d
+
+
+class TestCSC1d:
+
+    @staticmethod
+    def get_base_config():
+        return {
+            "lmbd": 0.1,
+            "n_components": 4,
+            "n_channels": 2,
+            "D_init": None,
+            "window": False,
+            "positive_D": True,
+            "positive_z": True,
+            "n_iterations": 20,
+            "deepcdl": False,
+            "random_state": None,
+            "device": torch.device("cpu"),
+            "dtype": float,
+        }
+
+    def test_init(self):
+        base_config = self.get_base_config()
+        csc = CSC1d(**base_config, kernel_size=(10,))
+        for k, v in base_config.items():
+            if k == "D_init":
+                continue
+            if k == "window":
+                assert csc.do_window == v
+                assert csc.window is None
+                continue
+            if k == "random_state":
+                assert type(csc.generator.seed()) is int
+                continue
+            assert getattr(csc, k) == v
