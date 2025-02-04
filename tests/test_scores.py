9138739a3868232965d9dd0454ea3a508323b5a4
tommoral <thomas.moreau.2010@gmail.com>
2025-02-05 00:26:16 +0100

    TST make them more informative

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -4,29 +4,33 @@ import pytest
 from rosecdl.utils.utils_exp import evaluate_D_hat
 
 
-@pytest.mark.parametrize("n_atoms", [5, 10, 20])
-@pytest.mark.parametrize("n_atoms_true", [5, 10, 20])
+@pytest.mark.parametrize("n_atoms", [5, 10])
+@pytest.mark.parametrize("extra_n_atoms", [0, 5])
 @pytest.mark.parametrize("n_channels", [1, 3])
 @pytest.mark.parametrize("height", [20, 35])
 @pytest.mark.parametrize("width", [20, 35])
-@pytest.mark.parametrize("height_true", [20, 35])
-@pytest.mark.parametrize("width_true", [20, 35])
+@pytest.mark.parametrize("size_increase", [0, 5])
 def test_dictionary_recovery_2d(
-    n_atoms, n_atoms_true, n_channels, height, width, height_true, width_true,
+    n_atoms, extra_n_atoms, n_channels, height, width, size_increase
 ):
-    if height_true > height or width_true > width:
-        pytest.skip("True dimensions are greater than learned dimensions")
-    if n_atoms_true > n_atoms:
-        pytest.skip("Number of true atoms is greater than number of atoms")
+    # One seed per config to avoid having too correlated tests
+    seed = hash((n_atoms, extra_n_atoms, n_channels, height, width, size_increase))
+    rng = np.random.RandomState(seed % (2 ** 32 -1))
 
-    rng = np.random.RandomState(0)
-
-    true_d = np.zeros((n_atoms_true, n_channels, height, width))
-    true_d[:, :, :10, :10] = 1
-    dict2 = rng.rand(n_atoms, n_channels, height, width)
+    true_d = rng.rand(n_atoms, n_channels, height, width)
+    true_d[:, :, :5, :5] = 3
+    dict2 = rng.rand(
+        n_atoms + extra_n_atoms, n_channels,
+        height + size_increase, width + size_increase
+    )
 
     random_score = evaluate_D_hat(true_d, dict2)
     assert random_score <= 0.2
 
     exact_score = evaluate_D_hat(true_d, true_d)
+    assert np.isclose(exact_score, 1.0)
+
+    true_d_extra = dict2.copy()
+    true_d_extra[:n_atoms, :, :height, :width] = true_d
+    exact_score = evaluate_D_hat(true_d, true_d_extra)
     assert np.isclose(exact_score, 1.0)
