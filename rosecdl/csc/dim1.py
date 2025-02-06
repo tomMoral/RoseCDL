96f330c4e5ebf6afb502458fbaf62ae3642031d3
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-02-06 11:10:20 +0100

    keep n_components, kernel_size, n_channels mandatory for rank1csc

--- rosecdl/csc/dim1.py
+++ rosecdl/csc/dim1.py
@@ -106,6 +106,57 @@ class CSC1d(ConvolutionalSparseCoder):
 class Rank1CSC1d(CSC1d):
     """1D Convolutional Sparse (en)Coder with rank-1 constraint."""
 
+    def __init__(
+        self,
+        lmbd: float,
+        n_components: int,
+        kernel_size: tuple,
+        n_channels: int,
+        D_init: torch.Tensor | None = None,
+        window: bool = False,
+        positive_D: bool = False,
+        positive_z: bool = True,
+        n_iterations: int = 30,
+        deepcdl: bool = False,
+        random_state: int | None = None,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        """Initialize the object.
+
+        Args:
+            lmbd: regularization parameter,
+            n_components: number of atoms in the dictionary,
+            kernel_size: support of the atoms (tuple of lenght n for n-dim signals),
+            n_channels: number of channels in the signal,
+            D_init: optional initial dictionary,
+            window: whether a tukey window should be applied to the atoms,
+            positive_D: whether to impose the atoms to have positive values,
+            positive_z: whether to impose the the activation vectors
+                to have positive values,
+            n_iterations: number of approximate sparse coding iteration,
+            deepcdl: True if unrolled sparse coding, else False,
+            random_state: seed for dictionary initialization and atom resampling,
+            device: device where the parameters are stored,
+            dtype: data type of the parameters.
+
+        """
+        super().__init__(
+            lmbd=lmbd,
+            n_components=n_components,
+            kernel_size=kernel_size,
+            n_channels=n_channels,
+            D_init=D_init,
+            window=window,
+            positive_D=positive_D,
+            positive_z=positive_z,
+            n_iterations=n_iterations,
+            deepcdl=deepcdl,
+            random_state=random_state,
+            device=device,
+            dtype=dtype,
+        )
+
     @property
     def uv_hat_(self):
         return get_uv(self.D_hat_)
