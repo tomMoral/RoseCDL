30d2d278291fbf38ca49ca14b7493867269058eb
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-03-31 14:54:03 +0200

    add arg conv_algo in CSC init method

--- rosecdl/csc/dim1.py
+++ rosecdl/csc/dim1.py
@@ -1,66 +1,24 @@
 import torch
-import torch.nn.functional as F
 from torch import fft, nn
+from torch.nn.functional import conv1d, conv_transpose1d, relu
 
 from rosecdl.csc.base import ConvolutionalSparseCoder
+from rosecdl.utils.convolution import fft_conv, fft_conv_transpose
 from rosecdl.utils.dictionary import get_uv, tukey_window
 
 
 class CSC1d(ConvolutionalSparseCoder):
     """Convolutional Sparse (en)Coder for 1D signals."""
 
-    def __init__(
-        self,
-        lmbd: float,
-        n_components: int | None = None,
-        kernel_size: tuple | None = None,
-        n_channels: int | None = None,
-        D_init: torch.Tensor | None = None,
-        window: bool = False,
-        positive_D: bool = False,
-        positive_z: bool = True,
-        n_iterations: int = 30,
-        deepcdl: bool = False,
-        random_state: int | None = None,
-        device: torch.device | None = None,
-        dtype: torch.dtype | None = None,
-    ):
-        """Initialize the object.
-
-        Args:
-            lmbd: regularization parameter,
-            n_components: number of atoms in the dictionary,
-            kernel_size: support of the atoms (tuple of lenght n for n-dim signals),
-            n_channels: number of channels in the signal,
-            D_init: optional initial dictionary,
-            window: whether a tukey window should be applied to the atoms,
-            positive_D: whether to impose the atoms to have positive values,
-            positive_z: whether to impose the the activation vectors
-                to have positive values,
-            n_iterations: number of approximate sparse coding iteration,
-            deepcdl: True if unrolled sparse coding, else False,
-            random_state: seed for dictionary initialization and atom resampling,
-            device: device where the parameters are stored,
-            dtype: data type of the parameters.
-
-        """
-        super().__init__(
-            lmbd=lmbd,
-            n_components=n_components,
-            kernel_size=kernel_size,
-            n_channels=n_channels,
-            D_init=D_init,
-            window=window,
-            positive_D=positive_D,
-            positive_z=positive_z,
-            n_iterations=n_iterations,
-            deepcdl=deepcdl,
-            random_state=random_state,
-            device=device,
-            dtype=dtype,
-        )
-        self.conv = F.conv1d
-        self.convt = F.conv_transpose1d
+    def set_conv_methods(self) -> None:
+        if self.conv_algo == "classical":
+            self.conv = conv1d
+            self.convt = conv_transpose1d
+        elif self.conv_algo == "fft":
+            self.conv = fft_conv
+            self.convt = fft_conv_transpose
+        else:
+            raise ValueError(f"Unknown convolution algorithm: {self.conv_algo}")
 
     def tukey_window(self) -> torch.Tensor:
         """N-dimensional Tukey window."""
@@ -73,7 +31,7 @@ class CSC1d(ConvolutionalSparseCoder):
         with torch.no_grad():
             if self.positive_D:
                 # Work on data as _D_hat is a nn.Parameter
-                self._D_hat.data = F.relu(self._D_hat.data)
+                self._D_hat.data = relu(self._D_hat.data)
 
             if self.do_window:
                 norm_atoms = torch.linalg.vector_norm(
@@ -112,57 +70,6 @@ class CSC1d(ConvolutionalSparseCoder):
 class Rank1CSC1d(CSC1d):
     """1D Convolutional Sparse (en)Coder with rank-1 constraint."""
 
-    def __init__(
-        self,
-        lmbd: float,
-        n_components: int,
-        kernel_size: tuple,
-        n_channels: int,
-        D_init: torch.Tensor | None = None,
-        window: bool = False,
-        positive_D: bool = False,
-        positive_z: bool = True,
-        n_iterations: int = 30,
-        deepcdl: bool = False,
-        random_state: int | None = None,
-        device: torch.device | None = None,
-        dtype: torch.dtype | None = None,
-    ):
-        """Initialize the object.
-
-        Args:
-            lmbd: regularization parameter,
-            n_components: number of atoms in the dictionary,
-            kernel_size: support of the atoms (tuple of lenght n for n-dim signals),
-            n_channels: number of channels in the signal,
-            D_init: optional initial dictionary,
-            window: whether a tukey window should be applied to the atoms,
-            positive_D: whether to impose the atoms to have positive values,
-            positive_z: whether to impose the the activation vectors
-                to have positive values,
-            n_iterations: number of approximate sparse coding iteration,
-            deepcdl: True if unrolled sparse coding, else False,
-            random_state: seed for dictionary initialization and atom resampling,
-            device: device where the parameters are stored,
-            dtype: data type of the parameters.
-
-        """
-        super().__init__(
-            lmbd=lmbd,
-            n_components=n_components,
-            kernel_size=kernel_size,
-            n_channels=n_channels,
-            D_init=D_init,
-            window=window,
-            positive_D=positive_D,
-            positive_z=positive_z,
-            n_iterations=n_iterations,
-            deepcdl=deepcdl,
-            random_state=random_state,
-            device=device,
-            dtype=dtype,
-        )
-
     @property
     def uv_hat_(self):
         return get_uv(self.D_hat_)
@@ -172,8 +79,8 @@ class Rank1CSC1d(CSC1d):
         with torch.no_grad():
             if self.positive_D:
                 # Work on data as u, v are nn.Parameter
-                self.u.data = F.relu(self.u.data)
-                self.v.data = F.relu(self.v.data)
+                self.u.data = relu(self.u.data)
+                self.v.data = relu(self.v.data)
 
             if self.do_window:
                 norm_col_v = torch.linalg.vector_norm(
@@ -212,6 +119,7 @@ class Rank1CSC1d(CSC1d):
 
         Args:
             D_init: tensor of shape (n_atoms, n_channels + n_times_atom)
+
         """
         D_hat = self.init_unnormalized_D(D_init)
 
