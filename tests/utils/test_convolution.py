efa5014b359c592e7a6945f16b10ad04d920d1eb
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-01 11:20:25 +0200

    add 1d tests

--- tests/utils/test_convolution.py
+++ tests/utils/test_convolution.py
@@ -1,8 +1,48 @@
 import pytest
 import torch
-from torch.nn.functional import conv2d, conv_transpose2d
+from torch.nn.functional import conv1d, conv2d, conv_transpose1d, conv_transpose2d
 
 from rosecdl.utils.convolution import fft_conv, fft_conv_transpose
+
+
+@pytest.mark.parametrize("batch_size", [2, 4, 8])
+@pytest.mark.parametrize("n_atoms", [2, 4, 10])
+@pytest.mark.parametrize("n_channels", [1, 3])
+@pytest.mark.parametrize("signal_size", [10, 20, 50, 100])
+@pytest.mark.parametrize("kernel_size", [3, 5, 9])
+@pytest.mark.parametrize("seed", [40, 41, 42])
+def test_fft_conv1d(batch_size, n_channels, signal_size, n_atoms, kernel_size, seed):
+    rng = torch.Generator()
+    rng.manual_seed(seed)
+
+    x = torch.rand(batch_size, n_channels, signal_size, generator=rng)
+    D = torch.rand(n_atoms, n_channels, kernel_size, generator=rng)
+
+    fft_res = fft_conv(x, D)
+    torch_res = conv1d(x, D)
+
+    assert torch.allclose(fft_res, torch_res)
+
+
+@pytest.mark.parametrize("batch_size", [2, 4, 8])
+@pytest.mark.parametrize("n_atoms", [2, 4, 10])
+@pytest.mark.parametrize("n_channels", [1, 3])
+@pytest.mark.parametrize("signal_size", [10, 20, 50, 100])
+@pytest.mark.parametrize("kernel_size", [3, 5, 9])
+@pytest.mark.parametrize("seed", [40, 41, 42])
+def test_fft_conv_transpose1d(
+    batch_size, n_channels, signal_size, n_atoms, kernel_size, seed
+):
+    rng = torch.Generator()
+    rng.manual_seed(seed)
+
+    z = torch.rand(batch_size, n_atoms, signal_size, generator=rng)
+    D = torch.rand(n_atoms, n_channels, kernel_size, generator=rng)
+
+    fft_res = fft_conv_transpose(z, D)
+    torch_res = conv_transpose1d(z, D)
+
+    assert torch.allclose(fft_res, torch_res, atol=1e-6)
 
 
 @pytest.mark.parametrize("batch_size", [2, 4, 8])
@@ -11,7 +51,7 @@ from rosecdl.utils.convolution import fft_conv, fft_conv_transpose
 @pytest.mark.parametrize("signal_size", [(10, 10), (20, 20), (50, 50), (100, 100)])
 @pytest.mark.parametrize("kernel_size", [(3, 3), (5, 5), (9, 9)])
 @pytest.mark.parametrize("seed", [40, 41, 42])
-def test_fft_conv(batch_size, n_channels, signal_size, n_atoms, kernel_size, seed):
+def test_fft_conv2d(batch_size, n_channels, signal_size, n_atoms, kernel_size, seed):
     rng = torch.Generator()
     rng.manual_seed(seed)
 
@@ -30,12 +70,11 @@ def test_fft_conv(batch_size, n_channels, signal_size, n_atoms, kernel_size, see
 @pytest.mark.parametrize("signal_size", [(10, 10), (20, 20), (50, 50), (100, 100)])
 @pytest.mark.parametrize("kernel_size", [(3, 3), (5, 5), (9, 9)])
 @pytest.mark.parametrize("seed", [40, 41, 42])
-def test_fft_conv_transpose(
+def test_fft_conv_transpose2d(
     batch_size, n_channels, signal_size, n_atoms, kernel_size, seed
 ):
-    print(locals())
     rng = torch.Generator()
-    rng.manual_seed(42)
+    rng.manual_seed(seed)
 
     z = torch.rand(batch_size, n_atoms, *signal_size, generator=rng)
     D = torch.rand(n_atoms, n_channels, *kernel_size, generator=rng)
