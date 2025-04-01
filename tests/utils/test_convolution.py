ecee76cc3ae27f4830b0ffe41a8b46a1a391491e
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-01 16:12:55 +0200

    write test for fft_conv_2d with different shapes

--- tests/utils/test_convolution.py
+++ tests/utils/test_convolution.py
@@ -50,15 +50,47 @@ def test_fft_conv_transpose1d(
 @pytest.mark.parametrize("batch_size", [2, 4, 8])
 @pytest.mark.parametrize("n_atoms", [2, 4, 10])
 @pytest.mark.parametrize("n_channels", [1, 3])
-@pytest.mark.parametrize("signal_size", [(10, 10), (20, 20), (50, 50), (100, 100)])
-@pytest.mark.parametrize("kernel_size", [(3, 3), (5, 5), (9, 9)])
-@pytest.mark.parametrize("seed", [40, 41, 42])
-def test_fft_conv2d(batch_size, n_channels, signal_size, n_atoms, kernel_size, seed):
+@pytest.mark.parametrize("signal_size", [(20, 20), (50, 50), (100, 100)])
+@pytest.mark.parametrize("kernel_size", [(5, 5), (9, 9)])
+@pytest.mark.parametrize("seed", [41, 42])
+def test_fft_conv2d_batch(
+    batch_size, n_channels, signal_size, n_atoms, kernel_size, seed
+):
     rng = torch.Generator()
     rng.manual_seed(seed)
 
     x = torch.rand(batch_size, n_channels, *signal_size, generator=rng)
     D = torch.rand(n_atoms, n_channels, *kernel_size, generator=rng)
+
+    fft_res = fft_conv(x, D)
+    torch_res = conv2d(x, D)
+
+    assert torch.allclose(fft_res, torch_res)
+
+
+@pytest.mark.parametrize("batch_size", [4])
+@pytest.mark.parametrize("n_atoms", [4])
+@pytest.mark.parametrize("n_channels", [3])
+@pytest.mark.parametrize("signal_rows", [10, 19, 50])
+@pytest.mark.parametrize("signal_cols", [10, 29, 50])
+@pytest.mark.parametrize("kernel_rows", [4, 6, 9])
+@pytest.mark.parametrize("kernel_cols", [3, 6, 9])
+@pytest.mark.parametrize("seed", [42])
+def test_fft_conv2d_shapes(
+    batch_size,
+    n_channels,
+    signal_rows,
+    signal_cols,
+    n_atoms,
+    kernel_rows,
+    kernel_cols,
+    seed,
+):
+    rng = torch.Generator()
+    rng.manual_seed(seed)
+
+    x = torch.rand(batch_size, n_channels, signal_rows, signal_cols, generator=rng)
+    D = torch.rand(n_atoms, n_channels, kernel_rows, kernel_cols, generator=rng)
 
     fft_res = fft_conv(x, D)
     torch_res = conv2d(x, D)
